124034501c1644b0eac1992105255458add3f332
orangefizz <nycik_pucik@mail.ru>
2016-12-05 20:05:01 +0300

    Work

--- CoordinateAxes.cpp
+++ CoordinateAxes.cpp
@@ -1,9 +1,16 @@
-#include "CoordinateAxes.h"
+ #include "Shapes.h"
 
 void CoordinateAxes::paint(Canvas &c) const
 {
-    // A working release coming soon!
-    // TODO
+
+    for(double i = c.bottomLeft().x; i<= c.topRight().x;i = i + c.pixelWidth())
+    {
+    c.setColor(i,0);
+    }
+    for(double j = c.bottomLeft().y; j<= c.topRight().y;j = j + c.pixelHeight())
+    {
+    c.setColor(0,j);
+    }
 }
 
 
