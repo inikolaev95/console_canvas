6a390e451a348c4bae090b2d7b0a9d3351d9219a
orangefizz <nycik_pucik@mail.ru>
2016-12-13 23:01:36 +0300

    Add files via upload

--- CoordinateAxes.cpp
+++ CoordinateAxes.cpp
@@ -3,14 +3,17 @@
 void CoordinateAxes::paint(Canvas &c) const
 {
 
-    for(double i = c.bottomLeft().x; i<= c.topRight().x;i = i + c.pixelWidth())
+    for(double i = c.bottomLeft().x+c.pixelWidth(); i< c.topRight().x;i = i + c.pixelWidth())
     {
-    c.setColor(i,0);
+        c.setColor(i,0, '-');
     }
-    for(double j = c.bottomLeft().y; j<= c.topRight().y;j = j + c.pixelHeight())
+    for(double j = c.bottomLeft().y+c.pixelHeight(); j< c.topRight().y;j = j + c.pixelHeight())
     {
-    c.setColor(0,j);
+        c.setColor(0,j,'|');
     }
+    c.setColor(c.bottomLeft().x,c.topRight().y,'^');
+    c.setColor(c.topRight().x,c.bottomLeft().y,'>');
+    c.setColor(c.bottomLeft().x,c.bottomLeft().y,'+');
 }
 
 
