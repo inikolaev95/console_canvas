9210c4aeec79560ff2008a7d80d49625e7a578c5
Alexey Zhuravlev <ctmstudent04@alt7.spbstu.ru>
2016-12-14 13:42:08 +0300

    Solved problems with left line

--- Rectangle.cpp
+++ Rectangle.cpp
@@ -36,8 +36,8 @@ void Rectangle::paint(Canvas& c) const
         for(int i=0;i<(ymax-ymin)/c.pixelHeight();i++)
             for(int j=0;j<(xmax-xmin)/c.pixelWidth();j++)
             {
-                if(i>0&&i<(ymax-ymin)/c.pixelHeight()-1&&j>0&&
-                        j<(xmax-xmin)/c.pixelWidth()-1)
+                if((i>0&&i<(ymax-ymin)/c.pixelHeight()-1)&&(j>1&&
+                        j<(xmax-xmin)/c.pixelWidth()-1))
                 {
                     c.setColor(xmin+j*c.pixelWidth(),ymin+i*c.pixelHeight(),this->fillColor());
                 }
@@ -46,6 +46,21 @@ void Rectangle::paint(Canvas& c) const
                     c.setColor(xmin+j*c.pixelWidth(),ymin+i*c.pixelHeight(),this->outlineColor());
                 }
             }
+        /*for(int i=0;i<(ymax-ymin)/c.pixelHeight();i++)
+        {
+                c.setColor(xmin+0*c.pixelWidth(),ymin+i*c.pixelHeight(),this->outlineColor());
+                c.setColor(xmin+((xmax-xmin)/c.pixelWidth()-1)*c.pixelWidth(),ymin+i*c.pixelHeight(),this->outlineColor());
+        }
+        for(int j=0;j<(xmax-xmin)/c.pixelWidth();j++)
+        {
+            c.setColor(xmin+j*c.pixelWidth(),ymin+0*c.pixelHeight(),this->outlineColor());
+            c.setColor(xmin+j*c.pixelWidth(),ymin+((ymax-ymin)/c.pixelHeight())*c.pixelHeight(),this->outlineColor());
+        }
+        for(int i=1;i<(ymax-ymin)/c.pixelHeight()-1;i++)
+            for(int j=2;j<(xmax-xmin)/c.pixelWidth()-1;j++)
+            {
+                c.setColor(xmin+j*c.pixelWidth(),ymin+i*c.pixelHeight(),this->fillColor());
+            }*/
 }
 
 void Rectangle::load(const VariantMap& m)
